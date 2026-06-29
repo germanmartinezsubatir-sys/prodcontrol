853122a7c1c408f537292ecf9337519a953a719e
germanmartinezsubatir-sys <germanmartinezsubatir@gmail.com>
2026-06-29 14:08:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-const CACHE='prodcontrol-v1';
-const FILES=['./','./index.html','./intro.mp4','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES.filter(f=>!f.includes('mp4'))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='prodcontrol-v2';
+const FILES=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
+  self.skipWaiting();
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));
+});
